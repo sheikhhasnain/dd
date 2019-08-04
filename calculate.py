842fe1468c2359cd17f73b666fe78dcece96c5f0
sheikhhasnain <52106344+sheikhhasnain@users.noreply.github.com>
2019-08-04 10:16:22 +0500

    cal

---
+++ calculate.py
@@ -0,0 +1,13 @@
+def calculate(answer, operator ,value):
+    if operator == "+":
+        answer = answer + value
+    if operator == "-":
+        answer = answer - value
+    if operator == "*":
+        answer = answer * value
+    if operator == "/":
+        answer = answer / value
+    else:
+        answer = answer
+        
+    return answer
